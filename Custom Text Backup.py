0356ac6393a33660687d08dec928a297cc9d5c08
Glutanimate <glutanimate@users.noreply.github.com>
2017-10-01 12:16:57 +0200

    Initial commit

---
+++ Custom Text Backup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Anki Add-on: Custom Text Backup
+
+Entry point for the add-on into Anki
+
+Please do not edit this if you do not know what you are doing.
+
+Copyright: (c) 2017 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+from __future__ import unicode_literals
+
+import custom_text_backup.main
---
+++ custom_text_backup/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of the Custom Text Backup add-on for Anki
+
+Main Module, hooks add-on methods into Anki
+
+Copyright: (c) 2017 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+from __future__ import unicode_literals
