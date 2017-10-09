158084a539508aa658141071a8fe0cf625033adb
Glutanimate <glutanimate@users.noreply.github.com>
2017-10-09 23:37:27 +0200

    Export of most values now working

--- custom_text_backup/main.py
+++ custom_text_backup/main.py
@@ -13,6 +13,8 @@ from __future__ import unicode_literals
 
 import sys, os
 import copy
+import io
+
 from aqt import mw
 from anki.utils import json
 from anki.hooks import addHook
@@ -30,7 +32,7 @@ if anki21:
 
 
 default_config = {
-    "searchTerm": "\"Note ID\":1* rated:350 Ba*",
+    "searchTerm": "",
     "noteTypeExceptions": {
         "Basic": ["Back", "Front"],
         "Cloze": ["Text"]
@@ -54,17 +56,17 @@ default_config = {
     "execAfterExport": ""
 }
 
-# snippet_format = """\
-# nid: {nid}
-# notetype: {notetype}
-# deck: {deck}
-# tags: {tags}
-# created: {created}
-# history: {history}
-# forecast: {forecast}
-# fieldnames: {fieldnames}
-# fields: {fields}\
-# """
+snippet_format = """\
+nid: {nid}
+notetype: {notetype}
+deck: {deck}
+tags: {tags_string}
+created: {created}
+history: {history_string}
+forecast: {forecast}
+fieldnames: {fieldnames_string}
+fields: {fields_string}\
+"""
 
 def getConfig():
     if anki21:
@@ -74,7 +76,7 @@ def getConfig():
         addon_path = os.path.dirname(__file__).decode(sys_encoding)
         config_path = os.path.join(addon_path, "config.json")
         try:
-            config = json.load(open(config_path))
+            config = json.load(io.open(config_path, encoding="utf-8"))
         except (IOError, OSError):
             config = None
     return config if config else {}
@@ -95,16 +97,17 @@ def performBackup():
     if pre_command:
         runCmd(pre_command)
     
-    export_path = config["exportPath"]
+    export_path = os.path.expanduser(config["exportPath"])
     query = config["searchTerm"]
     
+
     if not os.path.isdir(export_path):
         os.makedirs(export_path)
     
     individual_files = config["singleFilePerNote"]
     if not individual_files:
         out_file = os.path.join(
-            os.path.expanduser(export_path),
+            export_path,
             "anki_custom_backup.txt")
         write_mode = "a+"
     else:
@@ -114,43 +117,40 @@ def performBackup():
     individual_field_lines = config["singleLinePerField"]
 
 
-    fldsep = config["fieldSeparator"]
+    fldsep_format = config["fieldSeparator"]
     flds_start = config["fieldStarter"]
     flds_close = config["fieldCloser"]
-    notes = mw.col.findNotes(query)
+    nids = mw.col.findNotes(query)
     
-    for note in notes:
 
-        snippets = []
+    end = len(nids) - 1
+    for idx, nid in enumerate(nids):
+        note = mw.col.getNote(nid)
+        first_card = note.cards()[0]
+        model = note.model()
 
-        nid = ""
-        deck = ""
-        created = ""
+        nid = note.id
+        did = first_card.did
+        deck = mw.col.decks.name(did)
+        created = nid
 
-        tags = []
+        tags = note.tags
         history = []
         forecast = ""
-        fieldnames = []
-        fields = []
-        notetype = ""
+        fieldnames = mw.col.models.fieldNames(model)
+        fields = note.fields
+        notetype = model["name"]
 
-        format_fields = {var.__name__, var for var in 
-                            (nid, notetype, deck, tags, created)
-                        }
+        # TODO: switch to f-strings once Anki 2.0 becomes obsolete
+        format_fields = {
+            "nid": nid, "notetype": notetype,
+            "deck": deck, "created": created
+        }
 
-
-
-        # format_fields = {
-        #     "nid": nid,
-        #     "notetype": notetype,
-        #     "deck": deck,
-        #     "tags": tags,
-        #     "created": created,
-        #     "history": history,
-        #     "forecast": forecast,
-        #     "fieldnames": fieldnames,
-        #     "fields": fields,
-        # }
+        fldsep = fldsep_format.format(**format_fields)
+        tags_string = fldsep.join(tags)
+        history_string = fldsep.join(history)
+        fieldnames_string = fldsep.join(fieldnames)
 
         if individual_files:
             file_name = filename_format.format(**format_fields)
@@ -160,8 +160,8 @@ def performBackup():
             title_format = config["singleLineFieldTitle"]
         
             annotated_fields = [flds_start]
-            for fname, field in zip(fnames, fields):
-                title = title_format.format(**format_fields)
+            for fname, field in zip(fieldnames, fields):
+                title = title_format.format(fieldname=fname, **format_fields)
                 annotated_fields.append(title)
                 annotated_fields.append(field)
                 annotated_fields.append("")
@@ -171,14 +171,28 @@ def performBackup():
         else:
             fields_string = flds_start + fldsep.join(fields) + flds_close
 
-        fldsep = fldsep_format.format(**format_fields)
+        print(fields_string)
+        print(type(fields_string))
+        data_fields = {
+            "nid": nid, "notetype": notetype,
+            "deck": deck, "created": created,
+            "forecast": forecast,
+            "tags_string": tags_string,
+            "history_string": history_string, 
+            "fieldnames_string": fieldnames_string,
+            "fields_string": fields_string
+        }
 
+        print(data_fields)
+        print(snippet_format)
 
-
-        with open(out_file, write_mode) as f:
-            f.write("\n".join(snippets))
-
-
+        with io.open(out_file, write_mode, encoding="utf-8") as f:
+            res = snippet_format.format(**data_fields)
+            f.write(res)
+            if not individual_files and idx != end:
+                note_separator_format = config["noteSeparator"]
+                note_separator = note_separator_format.format(**format_fields)
+                f.write("\n" + note_separator + "\n")
 
 
     post_command = config.get("execBeforeExport", None)
